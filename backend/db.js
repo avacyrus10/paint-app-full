158a6757075a37c6ce90d26924819a32eda0d34c
avacyrus10 <avacyrus0@gmail.com>
2025-06-16 15:26:10 +0330

    update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,21 @@
-
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('paint.db');
-
 db.serialize(() => {
+
   db.run(`
     CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       username TEXT UNIQUE NOT NULL
     )
   `);
-});
 
-module.exports = db;
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS paintings (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      user_id INTEGER NOT NULL,
+      title TEXT,
+      shapes TEXT,
+      FOREIGN KEY(user_id) REFERENCES users(id)
+    )
+  `);
+});
 
